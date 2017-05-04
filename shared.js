b8a8b9b95a5b97133168f00b326a5e2184726275
poojagirish <pgir1@student.monash.edu>
2017-05-04 22:07:33 +0800

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,7 @@
+// Shared code needed by all three pages.
+
+// Prefix to use for Local Storage.  You may change this.
+var APP_PREFIX = "monash.eng1003.runChallengeApp";
+
+// Array of saved Run objects.
+var savedRuns = [];
